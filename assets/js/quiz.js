d444147ea82453f95849eefb1555992c409159dc
James Higgs <james.y.higgs@gmail.com>
2019-06-20 21:41:26 -0700

    third

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,19 +1,69 @@
-// Check Off Specific Todos By Clicking
-$("li").click(function(){
-    //if li is grey
-    if($(this).css("color") === "rgb(128, 128, 128)"){
-        //turn it black
-        $(this).css({
-            color: "black",
-            textDecoration: "none"
-    });
-}
-    //else
-    else {
-        //turn it grey
-        $(this).css({
-            color: "grey",
-            textDecoration: "line-through"
-        });
-    }
+let questionNum = 0;
+let score = 0;
+
+function startQuiz() {
+    $(".beginQuiz").on("click", ".startbutton", function(e) {
+        alert("yes");
+    $(".beginQuiz").remove();
+    $(".questionAnswer").css("display", "block");
+    $(".questionNum").text(1);
 });
+}
+
+function generateQuestion() {
+    if (questionNum < STORE.length) {
+        return `<div class="question-${questionNum}">
+        <h2>${STORE[questionNum].question}</h2>
+        <form>
+        <fieldset>
+        <br>
+        <label class="answerOption">
+        <input type="radio" value="${STORE[questionNum].answers[0]}" name="answer" required>
+        <span>${STORE[questionNum].answers[0]}</span>
+        </label>
+        </br>
+        <br>
+        <label class="answerOption">
+        <input type="radio" value="${STORE[questionNum].answers[1]}" name="answer" required>
+        <span>${STORE[questionNum].answers[1]}</span>
+        </label>
+        </br>
+        <br>
+        <label class="answerOption">
+        <input type="radio" value="${STORE[questionNum].answers[2]}" name="answer" required>
+        <span>${STORE[questionNum].answers[2]}</span>
+        </label>
+        </br>
+        <br>
+        <label class="answerOption">
+        <input type="radio" value="${STORE[questionNum].answers[3]}" name="answer" required>
+        <span>${STORE[questionNum].answers[3]}</span>
+        </label>
+        </br>
+        <button type="submit" class="submitButton">Submit</button>
+        </fieldset>
+        </form>
+        </div>`;
+    } else {
+        makeResults();
+        restartQuiz();
+        $(".questionNum").text(10)
+    }
+}
+
+function renderQuestion() {
+    $(".questionAnswer").html(generateQuestion());
+}
+
+
+
+
+
+function createQuiz () {
+    startQuiz();
+    /*renderQuestion();
+    userSelectAnswer();
+    renderNextQuestion();*/
+  }
+  
+  $(createQuiz);
